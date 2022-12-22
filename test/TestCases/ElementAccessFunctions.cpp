319f569451cd39ed93e6619c00149884851deee1
Janos Buttgereit <janos.buttgereit@sonible.com>
2022-12-22 14:08:07 +0100

    VctrBase: Added front and back

--- test/TestCases/ElementAccessFunctions.cpp
+++ test/TestCases/ElementAccessFunctions.cpp
@@ -28,22 +28,36 @@
 
 TEMPLATE_TEST_CASE ("Element access functions", "[VctrBase][Element Access]", bool, float, int64_t, std::string)
 {
-    auto v = UnitTestValues<TestType>::template vector<100, 0>();
-    auto a = UnitTestValues<TestType>::template array<100, 0>();
+    constexpr size_t size = 100;
+
+    auto v = UnitTestValues<TestType>::template vector<size, 0>();
+    auto a = UnitTestValues<TestType>::template array<size, 0>();
     auto s = vctr::Span (v);
 
-    const auto cv = UnitTestValues<TestType>::template vector<100, 0>();
-    const auto ca = UnitTestValues<TestType>::template array<100, 0>();
+    TestType* vPtr = v.data();
+    TestType* aPtr = a.data();
+    TestType* sPtr = s.data();
+
+    const auto cv = UnitTestValues<TestType>::template vector<size, 0>();
+    const auto ca = UnitTestValues<TestType>::template array<size, 0>();
     auto cs = vctr::Span (cv);
 
-    auto vFirst = *v.data();
-    auto vSecond = *(v.data() + 1);
-    auto aFirst = *a.data();
-    auto aSecond = *(a.data() + 1);
-    auto cvFirst = *cv.data();
-    auto cvSecond = *(cv.data() + 1);
-    auto caFirst = *ca.data();
-    auto caSecond = *(ca.data() + 1);
+    const TestType* cvPtr = cv.data();
+    const TestType* caPtr = ca.data();
+    const TestType* csPtr = cs.data();
+
+    auto vFirst = *vPtr;
+    auto vSecond = *(vPtr + 1);
+    auto vLast = *(vPtr + size - 1);
+    auto aFirst = *aPtr;
+    auto aSecond = *(aPtr + 1);
+    auto aLast = *(aPtr + size - 1);
+    auto cvFirst = *cvPtr;
+    auto cvSecond = *(cvPtr + 1);
+    auto cvLast = *(cvPtr + size - 1);
+    auto caFirst = *caPtr;
+    auto caSecond = *(caPtr + 1);
+    auto caLast = *(caPtr + size - 1);
 
     // operator[]
     REQUIRE (v[0] == vFirst);
@@ -72,6 +86,20 @@ TEMPLATE_TEST_CASE ("Element access functions", "[VctrBase][Element Access]", bo
     // Ensure that exceptions are thrown for at if the value is out of range
     REQUIRE_THROWS_AS (v.at (100), std::out_of_range);
     REQUIRE_THROWS_AS (a.at (100), std::out_of_range);
+
+    // front & back
+    REQUIRE (v.front() == vFirst);
+    REQUIRE (v.back() == vLast);
+    REQUIRE (a.front() == aFirst);
+    REQUIRE (a.back() == aLast);
+    REQUIRE (s.front() == vFirst);
+    REQUIRE (s.back() == vLast);
+    REQUIRE (cv.front() == cvFirst);
+    REQUIRE (cv.back() == cvLast);
+    REQUIRE (ca.front() == caFirst);
+    REQUIRE (ca.back() == caLast);
+    REQUIRE (cs.front() == cvFirst);
+    REQUIRE (cs.back() == cvLast);
 }
 
 TEMPLATE_TEST_CASE ("Iterators", "[VctrBase][Iterators]", float, int64_t, std::string)
