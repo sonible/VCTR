7ee151e395240769bd93a3bbf80e0ad80e6aa06f
JanosGit <JanosGit@users.noreply.github.com>
2023-09-11 11:24:04 +0000

    Deploying to gh-pages from  @ b3d7608ceb490eca6df446be91d53b1bf8f57c1a 🚀

--- search/typedefs_8.js
+++ search/typedefs_8.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['sse_0',['SSE',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#a7feb4e9a6c74f854370e03a558dd371d',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
-  ['sseret_1',['SSERet',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#ae804ad35ca50571002c160c1fa500877',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
-  ['ssesrc_2',['SSESrc',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#a513df8b0a4029502a83b4487704bccf4',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
-  ['storageinfotype_3',['StorageInfoType',['../namespacevctr.html#a222cc9e4338c24b2f0751c132503ce3a',1,'vctr']]]
+  ['spanstorageinfo_0',['SpanStorageInfo',['../namespacevctr.html#ad74ae35c2844e5e4e79889b55a82f21f',1,'vctr']]],
+  ['sse_1',['SSE',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#a7feb4e9a6c74f854370e03a558dd371d',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
+  ['sseret_2',['SSERet',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#ae804ad35ca50571002c160c1fa500877',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
+  ['ssesrc_3',['SSESrc',['../structvctr_1_1ExpressionTemplateBase_1_1ExpressionTypes.html#a513df8b0a4029502a83b4487704bccf4',1,'vctr::ExpressionTemplateBase::ExpressionTypes']]],
+  ['storageinfotype_4',['StorageInfoType',['../namespacevctr.html#a222cc9e4338c24b2f0751c132503ce3a',1,'vctr']]]
 ];
