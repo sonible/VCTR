927273c39868bc706560dba76daad3d9a3d8105e
Janos Buttgereit <janos.buttgereit@sonible.com>
2025-09-15 12:19:22 +0200

    SIMDFilter: Added useSIMD and assertSIMD filter expressions

--- include/vctr/Expressions/Filter/SIMDFilter.h
+++ include/vctr/Expressions/Filter/SIMDFilter.h
@@ -98,6 +98,103 @@ public:
     }
 };
 
+template <size_t extent, class SrcType>
+class AnySIMDFilter : public ExpressionTemplateBase
+{
+public:
+    using value_type = ValueType<SrcType>;
+
+    VCTR_COMMON_UNARY_EXPRESSION_MEMBERS (AnySIMDFilter, src)
+
+    VCTR_FORCEDINLINE constexpr value_type operator[] (size_t i) const
+    {
+        return src[i];
+    }
+
+    //==============================================================================
+    VCTR_FORWARD_PREPARE_SIMD_EVALUATION_UNARY_EXPRESSION_MEMBER_FUNCTIONS
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("sse4.1") SSERegister<value_type> getSSE (size_t i) const
+    requires (archX64 && has::getSSE<SrcType>)
+    {
+        return src.getSSE (i);
+    }
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("avx") AVXRegister<value_type> getAVX (size_t i) const
+    requires (archX64 && has::getAVX<SrcType> && is::realFloatNumber<value_type>)
+    {
+        return src.getAVX (i);
+    }
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("avx2") AVXRegister<value_type> getAVX (size_t i) const
+    requires (archX64 && has::getAVX<SrcType> && ! is::realFloatNumber<value_type>)
+    {
+        return src.getAVX (i);
+    }
+
+    NeonRegister<value_type> getNeon (size_t i) const
+    requires (archARM && has::getNeon<SrcType>)
+    {
+        return src.getNeon (i);
+    }
+};
+
+template <size_t extent, class SrcType>
+class AssertSIMDFilter : public ExpressionTemplateBase
+{
+public:
+    using value_type = ValueType<SrcType>;
+
+    VCTR_COMMON_UNARY_EXPRESSION_MEMBERS (AssertSIMDFilter, src)
+
+    VCTR_FORCEDINLINE constexpr value_type operator[] (size_t i) const
+    {
+        VCTR_ASSERT (false);
+        return src[i];
+    }
+
+    //==============================================================================
+    auto* data() const
+    requires has::data<SrcType>
+    {
+        return src.data();
+    }
+
+    VCTR_FORCEDINLINE const value_type* evalNextVectorOpInExpressionChain (value_type* dst) const
+    requires has::evalNextVectorOpInExpressionChain<SrcType, value_type>
+    {
+        VCTR_ASSERT (false);
+        return src.evalNextVectorOpInExpressionChain (dst);
+    }
+
+    //==============================================================================
+    VCTR_FORWARD_PREPARE_SIMD_EVALUATION_UNARY_EXPRESSION_MEMBER_FUNCTIONS
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("sse4.1") SSERegister<value_type> getSSE (size_t i) const
+    requires (archX64 && has::getSSE<SrcType>)
+    {
+        return src.getSSE (i);
+    }
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("avx") AVXRegister<value_type> getAVX (size_t i) const
+    requires (archX64 && has::getAVX<SrcType> && is::realFloatNumber<value_type>)
+    {
+        return src.getAVX (i);
+    }
+
+    VCTR_FORCEDINLINE VCTR_TARGET ("avx2") AVXRegister<value_type> getAVX (size_t i) const
+    requires (archX64 && has::getAVX<SrcType> && ! is::realFloatNumber<value_type>)
+    {
+        return src.getAVX (i);
+    }
+
+    NeonRegister<value_type> getNeon (size_t i) const
+    requires (archARM && has::getNeon<SrcType>)
+    {
+        return src.getNeon (i);
+    }
+};
+
 } // namespace vctr::expressions
 
 namespace vctr
@@ -134,4 +231,21 @@ constexpr inline ExpressionChainBuilder<expressions::NeonFilter> useNeonOrSSE;
 constexpr inline ExpressionChainBuilder<expressions::AVXFilter> useNeonOrAVX;
 constexpr inline ExpressionChainBuilder<expressions::SSEFilter> useNeonOrSSE;
 #endif
+
+/** This filter expression ensures that only SIMD based accelerated evaluation of the previous expression is possible.
+
+    In other words, this blocks any platform vector ops evaluation. If neither getSSE, getAVX nor getNEON is available
+    on the source, this will only forward the basic operator[] based interface.
+
+    @ingroup Expressions
+ */
+constexpr inline ExpressionChainBuilder<expressions::AnySIMDFilter> useSIMD;
+
+/** This filter inserts an assertion that will be hit in case that an expression is evaluated with anything but SIMD operations.
+
+    This is mainly helpful to ensure that your explicit SIMD based expressions do not accidentally run differently.
+
+    @ingroup Expressions
+ */
+constexpr inline ExpressionChainBuilder<expressions::AssertSIMDFilter> assertSIMD;
 } // namespace vctr
